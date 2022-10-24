a78f179ecb3f3f3d7d2b3397488cc8be7d8f9ff7
Sabuat Urbina <sabuaturbina@yahoo.com>
2022-10-24 01:44:52 -0300

    primer archivo

---
+++ exercise1.py
@@ -0,0 +1,18 @@
+farenheit = 90
+celsius = 0
+celsius = (farenheit - 32) * (5/9)
+print("The answer is ", celsius)
+
+#Segundo ejercicio
+
+varN = 6
+varF = 1
+varI = 1
+
+if varI < varN:
+    varI = varI + 1
+    varF = varF + varI
+    print(varF)
+else:
+    print("Final)")
+
